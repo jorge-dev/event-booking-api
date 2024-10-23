8cfa27326275cdf48dc6703f41475262fa23654b
jorge-dev <jo.luisa94@gmail.com>
2024-10-23 02:05:10 -0600

    Refactor routes to separate public and authenticated endpoints

--- routes/routes.go
+++ routes/routes.go
@@ -6,17 +6,21 @@ import (
 )
 
 func RegisterRoutes(server *gin.Engine) {
-	v1 := server.Group("/v1/api")
+	v1Public := server.Group("/v1/api")
 	{
-		v1.GET("/events", GetEvents)
-		v1.GET("/events/:id", GetEvent)
-		v1.POST("/events", middleware.ExtractEventAttributes(), CreateEvent)
-		v1.PUT("/events/:id", middleware.ExtractEventAttributes(), UpdateEvent)
-		v1.DELETE("/events/:id", DeleteEvent)
-
+		v1Public.GET("/events", GetEvents)
+		v1Public.GET("/events/:id", GetEvent)
 		// User routes
-		v1.POST("/signup", middleware.ExtractUserAttributes(), SignUp)
-		v1.POST("/login", middleware.ExtractAuthUserAttributes(), Login)
-
+		v1Public.POST("/signup", middleware.ExtractUserAttributes(), SignUp)
+		v1Public.POST("/login", middleware.ExtractAuthUserAttributes(), Login)
 	}
+
+	v1Auth := server.Group("/v1/api")
+	v1Auth.Use(middleware.Authenticate())
+	{
+		v1Auth.POST("/events", middleware.ExtractEventAttributes(), CreateEvent)
+		v1Auth.PUT("/events/:id", middleware.ExtractEventAttributes(), UpdateEvent)
+		v1Auth.DELETE("/events/:id", DeleteEvent)
+	}
+
 }
