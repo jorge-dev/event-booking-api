52b173fc45ad951b9be69641cdbc3dd1d6ae299a
jorge-dev <jo.luisa94@gmail.com>
2024-10-23 02:04:50 -0600

    Refactor jwt.go to use a configurable secret key for token generation and validation

--- utils/jwt.go
+++ utils/jwt.go
@@ -8,6 +8,8 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
+var secretKey = "secret"
+
 // GenerateToken generates a new JWT token
 func GenerateToken(email string, userId int64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
@@ -16,10 +18,40 @@ func GenerateToken(email string, userId int64) (string, error) {
 		"userId":     userId,
 		"exp":        time.Now().Add(time.Hour * 2).Unix(),
 	})
-	tokenString, err := token.SignedString([]byte("secret"))
+	tokenString, err := token.SignedString([]byte(secretKey))
 	if err != nil {
 		errorMessage := fmt.Sprintf("Error generating token: %v", err)
 		return "", errors.New(errorMessage)
 	}
 	return tokenString, nil
+}
+
+func ValidateToken(tokenString string) (int64, error) {
+	parsedToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secretKey), nil
+	})
+	if err != nil {
+		errorMessage := fmt.Sprintf("Error parsing token: %v", err)
+		return 0, errors.New(errorMessage)
+	}
+	if !parsedToken.Valid {
+		return 0, errors.New("invalid token")
+	}
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.New("error getting claims")
+	}
+
+	// Convert userId to int64
+	userIdFloat, ok := claims["userId"].(float64)
+	if !ok {
+		return 0, errors.New("userId claim is not a valid number")
+	}
+	userId := int64(userIdFloat)
+
+	return userId, nil
+
 }
