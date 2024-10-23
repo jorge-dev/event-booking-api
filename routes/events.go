1a2eb0a03d6af0e8f23559b9b9b8e89e0cb3fcf3
jorge-dev <jo.luisa94@gmail.com>
2024-10-23 02:13:08 -0600

    Refactor routes to handle user authorization and separate public and authenticated endpoints

--- routes/events.go
+++ routes/events.go
@@ -89,6 +89,14 @@ func CreateEvent(c *gin.Context) {
 // @response 200 - Event updated successfully with the event details.
 // @response 500 - Internal server error with an error message.
 func UpdateEvent(c *gin.Context) {
+
+	userId := c.GetInt64("userId")
+
+	if userId == 0 {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "User ID not found in context"})
+		return
+	}
+
 	event, exists := c.Get("event")
 	if !exists {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Event not found in context"})
@@ -101,15 +109,20 @@ func UpdateEvent(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event ID"})
 		return
 	}
-	_, err = models.GetByID(eventId)
+	eventFromDB, err := models.GetByID(eventId)
 	if err != nil {
 		errorMessage := "Could not find event with ID: " + strconv.FormatInt(eventId, 10)
 		c.JSON(http.StatusInternalServerError, gin.H{"message": errorMessage})
 		return
 	}
 
+	if eventFromDB.UserId != userId {
+		c.JSON(http.StatusForbidden, gin.H{"message": "You are not authorized to update this event"})
+		return
+	}
+
 	updatedEvent.ID = eventId
-	updatedEvent.UserId = 2233
+	updatedEvent.UserId = userId
 
 	err = updatedEvent.Update()
 	if err != nil {
@@ -133,6 +146,13 @@ func UpdateEvent(c *gin.Context) {
 // @response 500 - Internal server error with an error message.
 func DeleteEvent(c *gin.Context) {
 
+	userId := c.GetInt64("userId")
+
+	if userId == 0 {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "User ID not found in context"})
+		return
+	}
+
 	eventId, err := strconv.ParseInt(c.Param("id"), 10, 64)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event ID"})
@@ -142,6 +162,11 @@ func DeleteEvent(c *gin.Context) {
 	if err != nil {
 		errorMessage := "Could not find event with ID: " + strconv.FormatInt(eventId, 10)
 		c.JSON(http.StatusInternalServerError, gin.H{"message": errorMessage, "error": err.Error()})
+		return
+	}
+
+	if eventToDelete.UserId != userId {
+		c.JSON(http.StatusForbidden, gin.H{"message": "You are not authorized to delete this event"})
 		return
 	}
 
